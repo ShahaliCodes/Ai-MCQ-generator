a9e69a55e6cfdce9e2b0e57fbaacd2c646cea19f
ShahaliCodes <shahali44639@gmail.com>
2025-06-25 10:15:17 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 set -o errexit
 
-# Update package lists and install system dependencies
+# Install system dependencies
 apt-get update
 apt-get install -y \
     libjpeg-dev \
     zlib1g-dev \
     tesseract-ocr \
-    libtesseract-dev
+    libtesseract-dev \
+    python3.11-dev
+
+# Ensure we're using Python 3.11
+update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.11 1
+update-alternatives --set python3 /usr/bin/python3.11
 
 # Install Python dependencies
-pip install --upgrade pip
-pip install -r requirements.txt
+python3.11 -m pip install --upgrade pip
+python3.11 -m pip install -r requirements.txt
