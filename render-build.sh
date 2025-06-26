e42132b10a16127c3b0ce7bf771a9962c4a87931
ShahaliCodes <shahali44639@gmail.com>
2025-06-26 17:18:33 +0530

    Delete render-build.sh

--- render-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -o errexit
-
-# Reset environment
-pip uninstall -y numpy pandas
-
-# Upgrade pip and install fresh requirements
-pip install --upgrade pip
-pip install -r requirements.txt
